c4e5963ae1a1e89da2012f3a2349d64973d457e4
Paweł Łaba <labapawel@gmail.com>
2025-03-28 17:46:53 +0100

    zmiany

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -21,10 +21,34 @@ export class BoardComponent {
       }
     
   }
+
+
+  checkIfShip(index:number): boolean {
+     let res =true;
+     let rowIndex = index % this.cellsCount;
+     [-11, -10, -9, -1, 1, 9, 10, 11].forEach((i) => {
+      let el = index-i;
+      let row = el % this.cellsCount;
+    if((el >= 0 && el < this.cellsCount**2) && ((rowIndex==0 && row < 2) || (rowIndex==9 && row > 0))){
+          if(this.cells[el].ship) res = false;
+        }
+     });
+     return res;
+  }
+
   shipConflictsV(item: any, shipLength: number): boolean {
     let index = this.cells.indexOf(item);
     let res = false;
+    let slen = (index % this.cellsCount)-1;
+    console.log('--------------------------------------------');
+    console.log(slen, (slen + shipLength) < this.cellsCount);
+    if(!((slen + shipLength) < this.cellsCount)) 
+        return true;
+
+    console.log('--------------------------------------------');
+    
     for(let i = 0; i < shipLength; i++){
+      console.log(i, index + i, this.checkIfShip(index + i));
       if(this.cells[index + i].ship) res = true;
     }
     return res;
@@ -32,8 +56,10 @@ export class BoardComponent {
   shipConflictsH(item: any, shipLength: number): boolean {
     let index = this.cells.indexOf(item);
     let res = false;
+    
+
     for(let i = 0; i < shipLength; i+=this.cellsCount){
-      if(this.cells[index + i].ship) res = true;
+      if(this.cells[index + i].ship ) res = true;
     }
     
     return res;
